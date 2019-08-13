4e52255298bc0ff5731bf9f936d91c26677462a9
smerz1989 <stevennmerz@gmail.com>
2019-08-13 16:34:30 -0400

    Added function for changing monomer count in template files

--- src/simulation_class.py
+++ src/simulation_class.py
@@ -11,11 +11,16 @@ class Simulation(object):
         self.xyz_dir = xyz_dir
 
     def compile_simulation(self,packmol_path=os.path.abspath('/scratch/snm8xf/packmol/packmol')):
+        self.change_monomer_count()
         os.chdir(self.xyz_dir)
         sb.call([packmol_path],stdin=open('np.inp'))
         os.chdir(self.lt_dir)
         sb.call(["moltemplate.sh","-xyz",self.xyz_dir+"/np.xyz","-atomstyle","angle","system.lt"])
 
     def change_monomer_count(self):
+        num_monA = int(self.total_monomers*self.monomer_A_fraction)
+        num_monB = self.total_monomers-num_monA
         os.chdir(self.xyz_dir)
-		sb.call(["sed"])
+        sb.call(["sed","-i",'/abead/,/bbead/ s/number\ [0-9]\+/number\ '+str(num_monA)+'/g',"np.inp"])
+        sb.call(["sed","-i",'/bbead/, s/number\ [0-9]\+/number\ '+str(num_monB)+'/g',"np.inp"])
+
